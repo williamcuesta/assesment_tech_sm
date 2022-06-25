1e4c8db894bd5218785fcc453fa3919bb66084f9
william09y@gmail.com <william09y@hotmail.com>
2022-06-25 15:08:59 -0500

    base(first commit): of the assesment. in this case just check the headers.

---
+++ script_ETL.py
@@ -0,0 +1,15 @@
+
+import argparse
+import os 
+from sys import path
+import pandas as pd
+
+path.append(os.path.realpath('./'))
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    
+    main()
